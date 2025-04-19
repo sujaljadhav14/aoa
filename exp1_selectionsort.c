2b705e41788cc937bd88212db4a49f293452c964
Sujal Jadhav <166748974+sujaljadhav14@users.noreply.github.com>
2025-04-19 12:08:45 +0530

    Update exp1_selectionsort.c

--- exp1_selectionsort.c
+++ exp1_selectionsort.c
@@ -1,39 +1,30 @@
 #include <stdio.h>
-void selectionSort(int arr[], int n)
-{
-    int i, j, min_idx, temp;
-    for (i = 0; i < n - 1; i++)
-    {
-        min_idx = i;
-        for (j = i + 1; j < n; j++)
-        {
-            if (arr[j] < arr[min_idx])
-            {
-                min_idx = j;
-            }
-        }
-        temp = arr[min_idx];
-        arr[min_idx] = arr[i];
-        arr[i] = temp;
-    }
-}
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i = 0; i < size; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-}
+
 int main()
 {
     int arr[] = {64, 25, 12, 22, 11};
     int n = sizeof(arr) / sizeof(arr[0]);
-    printf("Unsorted array: \n");
-    printArray(arr, n);
-    selectionSort(arr, n);
-    printf("Sorted array: \n");
-    printArray(arr, n);
-    return 0;
+
+    // Selection Sort
+    for (int i = 0; i < n - 1; i++)
+    {
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+        }
+        // Swap
+        int temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
+
+    // Print Sorted Array
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    getch();
 }
