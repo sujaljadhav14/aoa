c032a1342d925030b1957b0839aa8cf5ee92e66f
Sujal Jadhav <166748974+sujaljadhav14@users.noreply.github.com>
2025-04-20 18:35:55 +0530

    Update exp3_mergesort.c

--- exp3_mergesort.c
+++ exp3_mergesort.c
@@ -1,74 +1,56 @@
 #include <stdio.h>
-void merge(int arr[], int left, int mid, int right)
-{
-    int i, j, k;
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
-    int leftArr[100], rightArr[100];
-    for (i = 0; i < n1; i++)
-    {
-        leftArr[i] = arr[left + i];
-    }
-    for (j = 0; j < n2; j++)
-    {
-        rightArr[j] = arr[mid + 1 + j];
-    }
-    i = 0;
-    j = 0;
-    k = left;
-    while (i < n1 && j < n2)
-    {
-        if (leftArr[i] <= rightArr[j])
-        {
-            arr[k] = leftArr[i];
-            i++;
-        }
-        else
-        {
-            arr[k] = rightArr[j];
-            j++;
-        }
-        k++;
-    }
-    while (i < n1)
-    {
-        arr[k] = leftArr[i];
-        i++;
-        k++;
-    }
-    while (j < n2)
-    {
-        arr[k] = rightArr[j];
-        j++;
-        k++;
-    }
-}
-void mergeSort(int arr[], int left, int right)
-{
-    if (left < right)
-    {
-        int mid = left + (right - left) / 2;
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-        merge(arr, left, mid, right);
-    }
-}
-void printArray(int arr[], int size)
-{
-    for (int i = 0; i < size; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-}
-int main()
+
+void main()
 {
     int arr[] = {12, 11, 13, 5, 6, 7};
-    int arrSize = sizeof(arr) / sizeof(arr[0]);
-    printf("Given array: ");
-    printArray(arr, arrSize);
-    mergeSort(arr, 0, arrSize - 1);
+    int n = sizeof(arr) / sizeof(arr[0]);
+}
+
+// Merge sort function inside main
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+        int m = (l + r) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+// Merge function inside main
+void merge(int arr[], int l, int m, int r)
+{
+    int i = 0, j = 0, k = l;
+    int n1 = m - l + 1, n2 = r - m;
+    int L[50], R[50];
+
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+    i = j = 0;
+
+    while (i < n1 && j < n2)
+        arr[k++] = (L[i] <= R[j]) ? L[i++] : R[j++];
+    while (i < n1)
+        arr[k++] = L[i++];
+    while (j < n2)
+        arr[k++] = R[j++];
+
+    // Print array function inside main
+    void print(int arr[], int size)
+    {
+        for (int i = 0; i < size; i++)
+            printf("%d ", arr[i]);
+        printf("\n");
+    }
+
+    printf("Original array: ");
+    print(arr, n);
+
+    mergeSort(arr, 0, n - 1);
+
     printf("Sorted array: ");
-    printArray(arr, arrSize);
-    return 0;
+    print(arr, n);
+    getch();
 }
